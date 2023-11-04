ab483b6bf12f7e5d9557f098b9cce1ecf2ac4392
gjones224 <gamingjones224@proton.me>
2023-11-04 15:06:26 -0400

    hello

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://worker-quiet-breeze-db45.tommy-a-mcelroy.workers.dev/",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTJhNzgxOGRhODNhYmRjMTc2ZmNiYjkyZmM2OTk3OCIsInN1YiI6IjY1NDY5MzI2ZmQ0ZjgwMDBjN2ZjMjIzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P944537aWm8vL-X7sNG8Q_P2WCi8YPw_X7uXOzoFbVA"
+};
